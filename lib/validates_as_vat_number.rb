375dadb95b2399c04eca50056fe226e8d9487208
Zachary Zadell <zacharyz@gmail.com>
2011-03-22 10:17:27 +0100

    changed service address updated country/vat regexp updated savon interface
    note this requires savon be used with httpi 0.7.6
    check_vat call to vies is disabled for now because it is buggy - relying only on the regular expression for vat validation

--- lib/validates_as_vat_number.rb
+++ lib/validates_as_vat_number.rb
@@ -9,19 +9,20 @@ module Develon
       }
       configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
       
-      structures = { 'AT' => /^ATU\w{8}$/, 'BE' => /^BE0\d{9}$/, 'BG' => /^BG\d{9,10}$/, 'CY' => /^CY\w{8}L$/,
-                     'CZ' => /^CZ\d{8,10}$/, 'DE' => /^DE\d{9}$/, 'DK' => /^DK\d{2}\s\d{2}\s\d{2}\s\d{2}$/,
-                     'EE' => /^EE\d{9}$/, 'EL' => /^EL\d{9}$/, 'ES' => /^ESX\d{7}X$/, 'FI' => /^FI\d{8}$/, 'FR' => /^FR\w{2}\s\d{9}$/,
-                     'GB' => /^GB(\d{3}\s\d{4}\s\d{2}|\d{3}\s\d{4}\s\d{2}\s\d{3}|GD\d{3}|HA\d{3})$/,
-                     'HU' => /^HU\d{8}$/, 'IE' => /^IE\wS\w{5}L$/, 'IT' => /^IT\d{11}$/, 'LT' => /^LT(\d{9}|\d{12})$/,
-                     'LU' => /^LU\d{8}$/, 'LV' => /^LV\d{12}$/, 'MT' => /^MT\d{8}$/, 'NL' => /^NL\w{9}B\w{2}$/,
-                     'PL' => /^PL\d{10}$/, 'PT' => /^PT\d{9}/, 'RO' => /^RO\d{2,10}$/, 'SE' => /^SE\d{12}$/, 'SI' => /^SI\d{8}$/,
-                     'SK' =>/^SK\d{10}$/ }
+      structures = {'AT' => /^ATU\w{8}$/, 'BE' => /^BE0\d{9}$/, 'BG' => /^BG\d{9,10}$/, 'CY' => /^CY\d{8}\w{1}$/,
+                          'CZ' => /^CZ\d{8,10}$/, 'DE' => /^DE\d{9}$/, 'DK' => /^DK\d{8}$/, 'EE' => /^EE\d{9}$/,
+                          'EL' => /^EL\d{9}$/, 'ES' => /^ES(\d{8}[A-Za-z]{1}|[A-Za-z]{1}\d{8}|[A-Za-z]{1}\d{7}[A-Za-z]{1})$/,
+                          'FI' => /^FI\d{8}$/, 'FR' => /^FR\w{2}\d{9}$/, 'GB' => /^GB(\d{9}|\d{12}|(GD|HA)\d{3})$/,
+                          'HU' => /^HU\d{8}$/, 'IE' => /^IE\d{1}[\w+*]{1}\d{5}[A-Za-z]{1}$/, 'IT' => /^IT\d{11}$/,
+                          'LT' => /^LT(\d{9}|\d{12})$/, 'LU' => /^LU\d{8}$/, 'LV' => /^LV\d{11}$/, 'MT' => /^MT\d{8}$/,
+                          'NL' => /^NL\d{9}B\d{2}$/, 'PL' => /^PL\d{10}$/, 'PT' => /^PT\d{9}/, 'RO' => /^RO\d{2,10}$/,
+                          'SE' => /^SE\d{12}$/, 'SI' => /^SI\d{8}$/, 'SK' =>/^SK\d{10}$/
+            }
 
-      validates_each(attr_names,configuration) do |record, attr_name, value|
+      validates_each(attr_names,configuration) do |record, attr_name, value| 
         country = country_code(value)
         if structures.include?(country)
-          message = configuration[:message] unless structures[country].match(value) && check_vat(country, value.gsub(/^\w\w/, ''))
+          message = configuration[:message] unless structures[country].match(value)
         else
           message = 'has an invalid country'
         end
@@ -32,13 +33,21 @@ module Develon
     protected
 
     def vies_driver
-      wsdl = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService?wsdl"
-      @driver = Savon::Client.new(wsdl)
+      @driver = Savon::Client.new do
+        #wsdl.document = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService?wsdl" 
+        wsdl.document = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl"
+      end
     end
+    
 
-    def check_vat(country_code, vat_number)
-      @driver = vies_driver unless @driver
-      @driver.check_vat { |soap| soap.body = { :country_code => country_code, :vat_number => vat_number } }.to_hash[:check_vat_response][:valid]
+    def check_vat(country_code, vat_number)  
+      @driver = vies_driver unless @driver  
+      #@driver.check_vat { |soap| soap.body = { :country_code => country_code, :vat_number => vat_number } }.to_hash[:check_vat_response][:valid]
+      response = @driver.request :check_vat do
+        soap.body = { :country_code => country_code, :vat_number => vat_number }
+      end   
+      
+      response.to_hash
     end
     
     def country_code(vat)
